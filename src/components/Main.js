de3fdd3b2e471f0bdda5c94fc267f94019c7e8c8
Peter G. Morales <pgmorales76@gmail.com>
2023-02-06 23:06:57 -0500

    Updated main component to display city data

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,9 +5,76 @@ import Restaurants from "./Restaurants";
 import Weather from "./Weather";
 import axios from "axios";
 
+
 class Main extends React.Component {
+
+    // importing props
+    constructor(props) {
+        super(props);
+
+        // setting state
+        this.state = {
+            display_info: false,
+            city: '',
+            city_data: {},
+            restaurant_data: [],
+            location_data: [],
+            weather_data: []
+
+        }
+    }
+
+    // search input data and sets the city
+    handle_search_input = e => {
+        let city_name = e.target.value;
+        this.setState({
+            city: city_name
+        },
+            () => console.log(this.state.city)
+        )
+
+    }
+
+    // displaying city data
+    handle_display_search = async (e) => {
+        e.preventDefault();
+
+        let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`
+
+        // waits for the response from the url and gives a response
+        let response = await axios.get(url);
+
+        console.log(response.data[0]);
+
+        // updated set state
+        this.setState({
+            display_info: true,
+            city_data: response.data[0]
+        })
+    }
+
     render() {
-        return (<p>Hello, World!!!</p>)
+        return (
+            <>
+                <Container>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Enter City</Form.Label>
+                            <Form.Control type="text" onInput={this.handle_search_input} />
+                        </Form.Group>
+                        <Button onClick={this.handle_display_search}>Explore!</Button>
+                    </Form>
+                </Container>
+
+                {this.state.display_info &&
+                    <>
+                        <h2>{this.state.city_data.display_name}</h2>
+                        <p>Latitude:{this.state.city_data.lat} Longitude: {this.state.city_data.lon}</p>
+                    </>
+                }
+
+            </>
+        )
     }
 }
 
