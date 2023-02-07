1de5f4094f5671995409bbd0f315abc0e5c42a1d
Peter G. Morales <pgmorales76@gmail.com>
2023-02-06 22:26:53 -0500

    Updated import statements

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { Button, Container, Form } from 
+import { Button, Container, Form } from "react-bootstrap";
+import Map from "./Map";
+import Restaurants from "./Restaurants";
+import Weather from "./Weather";
+import axios from "axios";
 
 class Main extends React.Component {
     render() {
