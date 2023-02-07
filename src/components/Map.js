aefca46af2a7f51fcf812ab7a36798514623d8f3
Peter G. Morales <pgmorales76@gmail.com>
2023-02-06 23:30:07 -0500

    Commented out some lines and added new map class

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
-class Map extends React.Component{
-    render() {
-        return(
-            <Container className="text-center">
-                <img src={this.props.map_image} alt={this.props.city} height={500}/>
-            </Container>
-        )
-    }
+class Map extends React.Component {
+  render() {
+    return (<p>Map Page</p>)
+  }
 }
+
+// class Map extends React.Component{
+//     render() {
+//         return(
+//             <Container className="text-center">
+//                 <img src={this.props.map_image} alt={this.props.city} height={500}/>
+//             </Container>
+//         )
+//     }
+// }
 
 export default Map;
