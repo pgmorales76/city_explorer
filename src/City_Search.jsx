d43f4f93c7ed4cd512e80206d9eab13bfefca80d
Peter G. Morales <pgmorales76@gmail.com>
2023-08-04 23:16:28 -0400

    Imported restaurant data and made some formatting changes

--- src/City_Search.jsx
+++ src/City_Search.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import city_map from "./assets/map.png";
+import restaurant_data from "./restaurants.json";
 
 class City_Search extends React.Component {
   constructor(props) {
@@ -16,13 +17,16 @@ class City_Search extends React.Component {
         <Container>
           <Form>
             <Form.Control type="text" placeholder="Enter city . . . " />
-            <Button onClick={() => this.setState({display_content: true})}>Explore!</Button>
+            <Button onClick={() => this.setState({ display_content: true })}>
+              Explore!
+            </Button>
           </Form>
 
-          {this.state.display_content ? <p>Hello, World!</p> : null }
+          {this.state.display_content ? <p>Hello, World!</p> : null}
           {this.state.display_content && <p>Hello, World 2.0!</p>}
-          {this.state.display_content && <img src={city_map} alt="map of Seattle" width={500}/>}
-
+          {this.state.display_content && (
+            <img src={city_map} alt="map of Seattle" width={500} />
+          )}
         </Container>
       </>
     );
